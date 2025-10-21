1df8883d39cb8589d0d8d92d81b7f4eb07f19b36
Josh Arnow <1476967+josharnow@users.noreply.github.com>
2025-10-21 15:22:54 -0400

    Redirect print output to stderr for MetricLogger to improve logging clarity

--- classification/furnace/utils.py
+++ classification/furnace/utils.py
@@ -15,6 +15,7 @@ import os
 import math
 import time
 import json
+import sys
 from collections import defaultdict, deque
 import datetime
 import numpy as np
@@ -226,18 +227,18 @@ class MetricLogger(object):
                         i, len(iterable), eta=eta_string,
                         meters=str(self),
                         time=str(iter_time), data=str(data_time),
-                        memory=torch.cuda.max_memory_allocated() / MB), flush=True)
+                        memory=torch.cuda.max_memory_allocated() / MB), flush=True, file=sys.stderr)
                 else:
                     print(log_msg.format(
                         i, len(iterable), eta=eta_string,
                         meters=str(self),
-                        time=str(iter_time), data=str(data_time)), flush=True)
+                        time=str(iter_time), data=str(data_time)), flush=True, file=sys.stderr)
             i += 1
             end = time.time()
         total_time = time.time() - start_time
         total_time_str = str(datetime.timedelta(seconds=int(total_time)))
         print('{} Total time: {} ({:.4f} s / it)'.format(
-            header, total_time_str, total_time / len(iterable)))
+            header, total_time_str, total_time / len(iterable)), file=sys.stderr)
 
 
 class TensorboardLogger(object):
