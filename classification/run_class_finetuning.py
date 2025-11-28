014ee76c5b8cbed7936e72c5d37ecb0a35ced45d
Josh Arnow <1476967+josharnow@users.noreply.github.com>
2025-11-28 16:09:48 -0500

    Modify focal loss alpha class weighting to 3x ([0.25, 0.75]) to force prioritization of minority class

--- classification/run_class_finetuning.py
+++ classification/run_class_finetuning.py
@@ -259,6 +259,10 @@ def get_args():
 
     parser.add_argument('--weight_cap', default=float('inf'), type=float, help='Cap for class alpha weights to avoid extreme values.')
 
+    parser.add_argument('--custom_majority_alpha', default=None, type=float, help='Custom alpha weight for majority class in Focal Loss.')
+
+    parser.add_argument('--custom_minority_alpha', default=None, type=float, help='Custom alpha weight for minority class in Focal Loss.')
+
 
     known_args, _ = parser.parse_known_args()
 
@@ -755,14 +759,14 @@ def main(args, ds_init):
         total_samples = sum(label_counts)
 
         # Applies a weight cap to avoid extremely high weights, which could destabilize training
-        # weight_cap = float('inf') # This can be adjusted based on experimentation (e.g., 100.0, 200.0, etc.)
         weight_cap = args.weight_cap # This can be adjusted based on experimentation (e.g., 100.0, 200.0, etc.)
 
         class_weights_list = [min((total_samples / (len(label_counts) * count)), weight_cap) for count in label_counts] if not args.sq_root_loss else [min((total_samples / (len(label_counts) * count))**0.5, weight_cap) for count in label_counts]
         
         # --- FIX: Use square root of inverse frequency to soften extreme weights ---
         # class_weights_list = [(total_samples / (len(label_counts) * count))**0.5 for count in label_counts]
-        class_weights = torch.tensor(class_weights_list, device=device)
+
+        class_weights = torch.tensor(class_weights_list if (not args.custom_majority_alpha or not args.custom_minority_alpha) else [args.custom_majority_alpha, args.custom_minority_alpha], device=device)
         # print("Using Class Weights for Loss:", class_weights)
 
         if mixup_fn is not None:
